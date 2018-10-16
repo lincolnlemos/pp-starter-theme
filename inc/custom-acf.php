2327aa41abf58645e1889835b4a5d632d0dfa7cc
Lincoln Lemos <hi@lincolnlemos.com>
2018-10-15 20:54:09 -0700

    acf-json from parent and child

--- inc/custom-acf.php
+++ inc/custom-acf.php
@@ -57,6 +57,27 @@
 /* ----------------------------------------- Valida CNPJ no ACF */  
 
 
+/** Add filters to ACF-JSON **/
+  
+  // Save at child theme
+  add_filter('acf/settings/save_json', function() {
+    return get_stylesheet_directory() . '/acf-json';
+  });
+
+  // Load from parent and child theme
+  add_filter('acf/settings/load_json', function($paths) {
+    $paths = array(get_template_directory() . '/acf-json');
+
+    if(is_child_theme())
+    {
+      $paths[] = get_stylesheet_directory() . '/acf-json';
+    }
+
+    return $paths;
+  });
+/** Add filters to ACF-JSON **/
+
+
 
 /* Cria página "Opções" para o ACF */
 /* ----------------------------------------- */
